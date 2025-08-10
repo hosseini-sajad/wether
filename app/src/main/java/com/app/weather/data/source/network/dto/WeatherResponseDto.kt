327c4806db4ed58f9429ecaaf66741b662ea52cf
Sajad <s.hosseini107@gmail.com>
2025-08-10 04:11:12 -0400

    Added model entities wrapper

--- app/src/main/java/com/app/weather/data/source/network/dto/WeatherResponseDto.kt
+++ app/src/main/java/com/app/weather/data/source/network/dto/WeatherResponseDto.kt
@@ -1,5 +1,9 @@
 package com.app.weather.data.source.network.dto
 
+import com.app.weather.data.model.MainEntity
+import com.app.weather.data.model.WeatherEntity
+import com.app.weather.data.model.WeatherResponseEntity
+
 data class WeatherResponseDto(
     val name: String,
     val main: MainDto,
@@ -13,4 +17,21 @@ data class MainDto(
 data class WeatherDto(
     val main: String,
     val icon: String
+)
+
+fun WeatherResponseDto.toEntity() = WeatherResponseEntity(
+    name = name,
+    mainEntity = main.toEntity(),
+    weatherEntity = weatherDto.map { it.toEntity() }
+)
+
+
+fun MainDto.toEntity() = MainEntity(
+    temp = temp
+)
+
+
+fun WeatherDto.toEntity() = WeatherEntity(
+    main = main,
+    icon = icon
 )
